5813032fc417ea99fb5d8f692a01f491603b6843
Marine Mellado <marine.mellado@gmail.com>
2022-06-21 14:04:57 +0200

    completed part 2

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request')
 
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
   res.render('login', {});
 });
 
 router.get('/weather', function (req, res, next) {
+  req.session.error = false;
   if (req.session.cityList == undefined) {
     req.session.cityList = []
   }
-  res.render('weather', { cityList: req.session.cityList });
+  res.render('weather', { cityList: req.session.cityList, error: req.session.error });
 });
 
 router.get('/delete-city', function (req, res, next) {
+  req.session.error = false;
   req.session.cityList.splice(req.query.index, 1)
-  res.render('weather', { cityList: req.session.cityList });
+  res.render('weather', { cityList: req.session.cityList, error: req.session.error });
 });
 
 router.post('/add-city', function (req, res, next) {
-  let city = {
-    name: req.body.city,
-    image: "/images/map.png",
-    icon: "fa-solid fa-cloud-rain",
-    info: "pluie",
-    tmin: 3.89,
-    tmax: 6,
-  }
 
-  if (typeof req.session.cityList !== "undefined" && req.session.cityList.length > 0) {
-    const isExisting = item => item.name === city.name
-    if (!(req.session.cityList.findIndex(isExisting) >= 0)) {
-      req.session.cityList.push(city)
+  var cityResult = request('GET', `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=74e78cd059bab6c7363618d5ebd7fe59&units=metric&lang=fr`)
+  var dataApi = JSON.parse(cityResult.body)
+  let error
+  if (dataApi.cod !== "404") {
+    req.session.error = false;
+    let city = {
+      name: dataApi.name,
+      image: "/images/map.png",
+      icon: `http://openweathermap.org/img/wn/${dataApi.weather[0].icon}@2x.png`,
+      info: dataApi.weather[0].description,
+      tmin: dataApi.main.temp_min,
+      tmax: dataApi.main.temp_max,
     }
+
+    if (typeof req.session.cityList !== "undefined" && req.session.cityList.length > 0) {
+      const isExisting = item => item.name.toLowerCase() === city.name.toLowerCase()
+      if (!(req.session.cityList.findIndex(isExisting) >= 0)) {
+        req.session.cityList.push(city)
+      }
+    } else {
+      req.session.cityList = [city]
+    }
+
   } else {
-    req.session.cityList = [city]
+    console.log("ville inconnue")
+    req.session.error = true;
   }
-  res.render('weather', { cityList: req.session.cityList });
+  res.render('weather', { cityList: req.session.cityList, error: req.session.error });
 });
 
 module.exports = router;
