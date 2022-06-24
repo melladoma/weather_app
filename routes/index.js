8bd160b14e2e184a65dac15aae150e55fd8dd039
Marine Mellado <marine.mellado@gmail.com>
2022-06-24 16:30:39 +0200

    fixed routes bug

--- routes/index.js
+++ routes/index.js
@@ -2,60 +2,12 @@ var express = require('express');
 var router = express.Router();
 var request = require('sync-request');
 var CityModel = require('../models/cities');
-var UserModel = require('../models/users');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log(req.session.user)
   res.render('login', {});
-});
-
-//POST sign-up
-router.post('/sign-up', async function (req, res, next) {
-
-  var newUser = new UserModel({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  var existingUser = await UserModel.findOne({ email: newUser.email })
-  if (!existingUser) {
-    //instead of (existingUser === null)
-    var userSaved = await newUser.save();
-    req.session.user = {
-      username: userSaved.username,
-      userId: userSaved._id.toString(),
-    }
-    console.log(req.session.user)
-
-    res.redirect('/weather');
-  } else {
-    res.redirect('/')
-  }
-
-});
-
-//POST sign-in
-router.post('/sign-in', async function (req, res, next) {
-  var existingUser = await UserModel.findOne({ email: req.body.email, password: req.body.password })
-  if (existingUser !== null) {
-
-    req.session.user = {
-      username: existingUser.username,
-      userId: existingUser._id.toString(),
-    }
-
-    res.redirect('/weather');
-  } else {
-    res.redirect('/')
-  }
-});
-
-router.get('/logout', function (req, res, next) {
-  req.session.user = null;
-  console.log(req.session.user)
-
-  res.redirect('/')
 });
 
 //GET cities page
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require('../models/users');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', {});
+});
+
+//POST sign-up
+router.post('/sign-up', async function (req, res, next) {
+  var newUser = new UserModel({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  var existingUser = await UserModel.findOne({ email: newUser.email })
+  if (!existingUser) {
+    //instead of (existingUser === null)
+    var userSaved = await newUser.save();
+    req.session.user = {
+      username: userSaved.username,
+      userId: userSaved._id.toString(),
+    }
+    console.log(req.session.user)
+
+    res.redirect('/weather');
+  } else {
+    res.redirect('/')
+  }
+
+});
+
+//POST sign-in
+router.post('/sign-in', async function (req, res, next) {
+  var existingUser = await UserModel.findOne({ email: req.body.email, password: req.body.password })
+  console.log(existingUser)
+  if (existingUser) {
+    req.session.user = {
+      username: existingUser.username,
+      userId: existingUser._id.toString(),
+    }
+    res.redirect('/weather');
+  } else {
+    res.redirect('/')
+  }
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session.user = null;
+  console.log(req.session.user)
+
+  res.redirect('/')
 });
 
 module.exports = router;
